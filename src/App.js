8bb176fc80c0390e79ac88d080a14825753a6be6
balldev1 <balldev1@hotmail.com>
2023-05-25 14:21:56 +0700

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import Testimonials from './assets/Testimonials/Testimonials';
+import Footer from './components/Footer/Footer';
+import Hero from './components/Hero/Hero';
+import Join from './components/Join/Join';
+import Planscard from './components/Planscard/Planscard';
+import Programs from './components/Programs/Programs';
+import Reasons from './components/Reasons/Reasons';
+
 function App() {
+
+
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+          <Hero/>
+          <Programs/>
+          <Reasons/>
+          <Planscard/>
+          <Testimonials/>
+          <Join/>
+          <Footer/>
+
     </div>
   );
 }
